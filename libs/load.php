0c3bccd578e827cbc30a4345241ced41033bd8dc
SignpostMarv <github@signpostmarv.name>
2011-12-01 21:57:00 +0000

    Missing a required file for is_email() to work

--- libs/load.php
+++ libs/load.php
@@ -2,6 +2,7 @@
 //!	This is where we load all PHP libs we explicitly need.
 namespace{
 	require_once('Aurora/load.php');
+	require_once('backpress/includes/functions.plugin-api.php'); // we need this for the is_email() function.
 	require_once('backpress/includes/functions.formatting.php'); // we need this for the is_email() function.
 }
 ?>
