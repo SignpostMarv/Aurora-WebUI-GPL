3c1d1f5d2651dc707c5effa1b00d5b6cf53dd896
SignpostMarv <github@signpostmarv.name>
2011-12-04 22:14:05 +0000

    user-editable files go in .gitignore, example files go in the repository

---
+++ config.example.php
@@ -0,0 +1,20 @@
+<?php
+namespace{
+	date_default_timezone_set('Europe/London'); // this is just to get rid of pesky errors
+
+	header('Content-Type: text/plain');
+
+	require_once('libs/load.php');
+
+	use IHateGlobals as Globals;
+	use Aurora\Addon\WebUI;
+	use Aurora\Addon\WebUI\Configs;
+
+	$configs = Configs::i();
+
+	$configs[] = WebUI::r(
+		'http://localhost:8007/WIREDUX',
+		'Password'
+	);
+}
+?>
