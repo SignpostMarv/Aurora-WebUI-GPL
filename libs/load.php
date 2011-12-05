35a7bec270de0273e2acc32a6deaddf523eacb19
SignpostMarv <github@signpostmarv.name>
2011-12-05 22:28:41 +0000

    Implementing an interface to the pomo lib in backPress

--- libs/load.php
+++ libs/load.php
@@ -1,8 +1,16 @@
 <?php
 //!	This is where we load all PHP libs we explicitly need.
 namespace{
-	require_once('Aurora/load.php');
 	require_once('backpress/includes/functions.plugin-api.php'); // we need this for the is_email() function.
 	require_once('backpress/includes/functions.formatting.php'); // we need this for the is_email() function.
+	require_once('backpress/includes/functions.bp-options.php'); // we need this for the esc_attr() function.
+	require_once('backpress/includes/pomo/mo.php'); // we need this for the esc_attr() function.
+	require_once('Aurora/load.php');
+
+	use Aurora\Addon\WebUI;
+
+	function __($text, $domain = 'default') {
+		return WebUI\pomo::i()->get_translations($domain)->translate($text);
+	}
 }
 ?>
