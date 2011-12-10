3201fb052dd72bc523bfe667d66e1e29d6a46cf7
SignpostMarv <github@signpostmarv.name>
2011-12-10 21:50:29 +0000

    making login form a plugin hook

--- plugins/logged-in/logged-in.php
+++ plugins/logged-in/logged-in.php
@@ -12,6 +12,7 @@ Author URI: https://github.com/SignpostMarv/
 namespace Aurora\Addon\WebUI\plugins\logged_in{
 
 	use Globals;
+	use Aurora\Addon\WebUI\Configs;
 	use Aurora\Addon\WebUI\Template;
 
 
@@ -27,6 +28,44 @@ namespace Aurora\Addon\WebUI\plugins\logged_in{
 	}
 
 
+	function login_form(){
+?>
+		<form method=post>
+<?php
+		do_action('pre_login_form_fieldset');
+?>
+			<fieldset>
+				<legend><?php echo esc_html(__('Grid')); ?></legend>
+<?php	echo wp_kses(apply_filters('login_form_hidden_input', ''), array('input'=>array('type'=>array('hidden'), 'name'=>array(), 'value'=>array()))),"\n"; ?>
+				<select name=grid>
+<?php
+	Configs::i()->rewind();
+	foreach(Configs::i() as $k=>$webui){ ?>
+					<option value="<?php echo esc_attr($k); ?>"<?php if(Configs::d() === $webui){?> selected <?php } ?>><?php echo esc_html($webui->get_grid_info('gridnick')); ?></option>
+<?php } ?>
+				</select>
+			</fieldset>
+<?php
+		do_action('post_login_form_fieldset');
+		do_action('pre_account_credentials_fieldset');
+?>
+			<fieldset>
+				<legend><?php echo esc_html(__('Account Credentials')); ?></legend>
+				<ol>
+					<li><label for=username><?php echo esc_html(__('Username')); ?>: </label><input id=username name=username<?php if(isset($_POST['username'])){ echo ' value="',esc_attr($_POST['username']),'" '; } ?>></li>
+					<li><label for=password><?php echo esc_html(__('Password')); ?>: </label><input id=password name=password type=password></li>
+				</ol>
+				<button type=submit><?php echo esc_html(__(apply_filters('login_form_button_login','Login'))); ?></button>
+			</fieldset>
+<?php
+		do_action('post_account_credentials_fieldset');
+?>
+		</form>
+<?php
+	}
+
+
 	add_filter('main_nav_links', __NAMESPACE__ . '\main_nav_links', 10, 1);
+	add_action('login_form', __NAMESPACE__ . '\login_form');
 }
 ?>
--- templates/default/login.php
+++ templates/default/login.php
@@ -15,26 +15,7 @@
 ?>
 	<section>
 		<h1><?php echo esc_html(__('Login')); ?></h1>
-		<form method=post>
-			<fieldset>
-				<legend><?php echo esc_html(__('Grid')); ?></legend>
-				<select name=grid>
-<?php
-	Configs::i()->rewind();
-	foreach(Configs::i() as $k=>$webui){ ?>
-					<option value="<?php echo esc_attr($k); ?>"<?php if(Configs::d() === $webui){?> selected <?php } ?>><?php echo esc_html($webui->get_grid_info('gridnick')); ?></option>
-<?php } ?>
-				</select>
-			</fieldset>
-			<fieldset>
-				<legend><?php echo esc_html(__('Account Credentials')); ?></legend>
-				<ol>
-					<li><label for=username><?php echo esc_html(__('Username')); ?>: </label><input id=username name=username<?php if(isset($_POST['username'])){ echo ' value="',esc_attr($_POST['username']),'" '; } ?>></li>
-					<li><label for=password><?php echo esc_html(__('Password')); ?>: </label><input id=password name=password type=password></li>
-				</ol>
-				<button type=submit><?php echo esc_html(__('Login')); ?></button>
-			</fieldset>
-		</form>
+<?php	do_action('login_form'); ?>
 	</section>
 <?php
 	require_once('_footer.php');
