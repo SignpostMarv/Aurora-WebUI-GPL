874ceed76a795ca004fb2024ec8092988f4d60e2
SignpostMarv <github@signpostmarv.name>
2011-12-17 17:37:09 +0000

    documentation, adding an abstract seekable Iterator to be used by WebUI method responses

--- libs/Aurora/Addon/WebUI/abstracts.php
+++ libs/Aurora/Addon/WebUI/abstracts.php
@@ -6,9 +6,13 @@
 namespace Aurora\Addon\WebUI{
 
 	use Iterator;
+	use SeekableIterator;
 	use Countable;
 	use ArrayAccess;
 
+	use Aurora\Addon\WebUI;
+
+//!	abstract iterator, not for any particular class but we don't want to duplicate code.
 	abstract class abstractIterator implements Iterator, Countable{
 //!	array holds the values of the iterator class
 		protected $data = array();
@@ -38,11 +42,12 @@ namespace Aurora\Addon\WebUI{
 		}
 	}
 
+//!	abstract iterator for instances of Aurora::Addon::WebUI::abstractUser
 	abstract class abstractUserIterator extends abstractIterator{
 
 //!	public constructor
 /**
-*	Since Aurora::Addon::WebUI::abstractUserIterator does not implement methods for appending values, calling the constructor with no arguments is a shorthand means of indicating there are no abstract users available.
+*	Since Aurora::Addon::WebUI::abstractUserIterator does not implement methods for appending values, calling the constructor with no arguments is a shorthand means of indicating there are no users available.
 *	@param mixed $archives an array of Aurora::Addon::WebUI::abstractUser instances or NULL
 */
 		public function __construct(array $archives=null){
@@ -87,6 +92,70 @@ namespace Aurora\Addon\WebUI{
 */
 		public function valueOffset($value){
 			return array_search($value, $this->data);
+		}
+	}
+
+//!	abstract seekable iterator
+	abstract class abstractSeekableIterator extends abstractIterator{
+
+//!	object instance of Aurora::Addon::WebUI
+		protected $WebUI;
+
+
+		protected function __construct(WebUI $WebUI, $start=0, $total=0){
+			if(is_integer($total) === false){
+				throw new InvalidArgumentException('Total number of entities must be an integer.');
+			}else if($total < 0){
+				throw new InvalidArgumentException('Total number of entities must be greater than or equal to zero.');
+			}
+			$this->WebUI = $WebUI;
+			$this->total = $total;
+			$this->seek($start);
+		}
+
+//!	integer total number of groups
+		protected $total;
+
+//!	@return integer
+		public function count(){
+			return $this->total;
+		}
+
+//!	@return integer
+		public function key(){
+			return ($this->pos < $this->count()) ? $this->pos : null;
+		}
+
+//!	@return bool TRUE if the current cursor position is valid, FALSE otherwise.
+		public function valid(){
+			return ($this->key() !== null);
+		}
+
+//!	advance the cursor
+		public function next(){
+			++$this->pos;
+		}
+
+//!	integer cursor position
+		protected $pos=0;
+
+//!	Move the cursor to the specified point.
+		public function seek($to){
+			if(is_string($to) === true && ctype_digit($to) === true){
+				$to = (integer)$to;
+			}
+			if(is_integer($to) === true && $to < 0){
+				$to = abs($to) % $this->count();
+				$to = $this->count() - $to;
+			}
+
+			if(is_integer($to) === false){
+				throw new InvalidArgumentException('Seek point must be an integer.');
+			}else if($to >= $this->count()){
+				throw new LengthException('Cannot seek past Aurora::Addon::WebUI::abstractSeekableIterator::count()');
+			}
+
+			$this->pos = $to;
 		}
 	}
 }
