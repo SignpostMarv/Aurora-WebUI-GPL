7e7beefe3c623803d4540f22a11344d7d58d1476
SignpostMarv <github@signpostmarv.name>
2011-12-21 19:22:33 +0000

    abstracting out some functionality of the GetGroupRecords iterator

--- libs/Aurora/Addon/WebUI/abstracts.php
+++ libs/Aurora/Addon/WebUI/abstracts.php
@@ -158,5 +158,63 @@ namespace Aurora\Addon\WebUI{
 			$this->pos = $to;
 		}
 	}
+
+
+	abstract class abstractSeekableFilterableIterator extends abstractSeekableIterator{
+
+//!	mixed either NULL indicating no sort filters, or an array of field name keys and boolean values indicating sort order.
+		private $sort;
+
+//!	mixed either NULL indicating no boolean filters, or an array of field name keys and boolean values.
+		private $boolFields;
+
+	//!	Because we use a seekable iterator, we hide the constructor behind a registry method to avoid needlessly calling the end-point if we've rewound the iterator, or moved the cursor to an already populated position.
+/**
+*	@param object $WebUI instance of Aurora::Addon::WebUI We need to specify this in case we want to iterate past the original set of results.
+*	@param integer $start initial cursor position
+*	@param integer $total Total number of results possible with specified filters
+*	@param array $sort optional array of field names for keys and booleans for values, indicating ASC and DESC sort orders for the specified fields.
+*	@param array $boolFields optional array of field names for keys and booleans for values, indicating 1 and 0 for field values.
+*	@param array $groups if specified, should be an array of instances of Aurora::Addon::WebUI::GroupRecord that were pre-fetched with a call to the API end-point.
+*/
+		protected function __construct(WebUI $WebUI, $start=0, $total=0, array $sort=null, array $boolFields=null){
+			parent::__construct($WebUI, $start, $total);
+			$this->sort = $sort;
+			$this->boolFields = $boolFields;
+		}
+
+//! This is a registry method for a class that implements the SeekableIterator class, so we can save ourselves some API calls if we've already fetched some groups.
+/**
+*	@param object $WebUI instance of Aurora::Addon::WebUI We need to specify this in case we want to iterate past the original set of results.
+*	@param integer $start initial cursor position
+*	@param integer $total Total number of results possible with specified filters
+*	@param array $sort optional array of field names for keys and booleans for values, indicating ASC and DESC sort orders for the specified fields.
+*	@param array $boolFields optional array of field names for keys and booleans for values, indicating 1 and 0 for field values.
+*	@param array $entities if specified, should be an array of entity objects to be validated by the child constructor
+*/
+		public static function r(WebUI $WebUI, $start=0, $total=0, array $sort=null, array $boolFields=null, array $entities=null){
+			static $registry = array();
+			$hash1 = spl_object_hash($WebUI);
+			$hash2 = md5(print_r($sort,true));
+			$hash3 = md5(print_r($boolFields,true));
+
+			if(isset($registry[$hash1]) === false){
+				$registry[$hash1] = array();
+			}
+			if(isset($registry[$hash1][$hash2]) === false){
+				$registry[$hash1][$hash2] = array();
+			}
+
+			$create = (isset($registry[$hash1][$hash2][$hash3]) === false) || ($create === false && $registry[$hash1][$hash2][$hash3]->count() !== $total);
+
+			if($create === true){
+				$registry[$hash1][$hash2][$hash3] = new static($WebUI, $start, $total, $sort, $boolFields, $entities);
+			}
+
+			$registry[$hash1][$hash2][$hash3]->seek($start);
+
+			return $registry[$hash1][$hash2][$hash3];
+		}
+	}
 }
 ?>
