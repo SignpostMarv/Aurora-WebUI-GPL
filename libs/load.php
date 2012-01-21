284f85ca243c2218e8f7e1a714df334879573a85
SignpostMarv <github@signpostmarv.name>
2012-01-21 07:48:08 +0000

    swapping PHP files for gzipped phar

--- libs/load.php
+++ libs/load.php
@@ -1,11 +1,11 @@
 <?php
 //!	This is where we load all PHP libs we explicitly need.
 namespace{
-	require_once('backpress/includes/functions.plugin-api.php'); // we need this for the is_email() function.
-	require_once('backpress/includes/functions.formatting.php'); // we need this for the is_email() function.
-	require_once('backpress/includes/functions.bp-options.php'); // we need this for the esc_attr() function.
-	require_once('backpress/includes/functions.kses.php'); // we need this for the wp_kses() function.
-	require_once('backpress/includes/pomo/mo.php'); // we need this for the esc_attr() function.
+	require_once('phar://../libs/backpress-full.phar.gz/includes/functions.plugin-api.php'); // we need this for the is_email() function.
+	require_once('phar://../libs/backpress-full.phar.gz/includes/functions.formatting.php'); // we need this for the is_email() function.
+	require_once('phar://../libs/backpress-full.phar.gz/includes/functions.bp-options.php'); // we need this for the esc_attr() function.
+	require_once('phar://../libs/backpress-full.phar.gz/includes/functions.kses.php'); // we need this for the wp_kses() function.
+	require_once('phar://../libs/backpress-full.phar.gz/includes/pomo/mo.php'); // we need this for the esc_attr() function.
 	require_once('recaptcha/recaptchalib.php'); // we need this for the esc_attr() function.
 	require_once('Aurora/load.php');
 
