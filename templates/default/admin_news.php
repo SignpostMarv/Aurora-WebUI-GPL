7762f4624f0b32216ced6ea367e02bebbee2014e
SignpostMarv <github@signpostmarv.name>
2012-04-30 00:56:20 +0100

    adding an id attribute to the table row corresponding to the news item

--- templates/default/admin_news.php
+++ templates/default/admin_news.php
@@ -21,7 +21,7 @@ if($news->count() >= 1){
 <?php
 	foreach(Globals::i()->WebUI->NewsFromGroupNotices(0, $news->count()) as $newsItem){
 ?>
-				<tr>
+				<tr id="news_<?php echo esc_attr($newsItem->NoticeID()); ?>">
 					<th scope=row headers=news-manager-title><?php echo esc_html($newsItem->Subject()); ?></th>
 					<td headers=news-manager-date><time datetime="<?php echo esc_attr(date('r', $newsItem->Timestamp())); ?>"><?php echo esc_html(date("l M d Y", $newsItem->Timestamp())); ?></time></td>
 					<td headers=news-manager-action><a href="<?php echo esc_attr(Template\link('/admin/news/edit/?id=' . Template\squishUUID($newsItem->NoticeID()))); ?>"><?php echo esc_html(__('Edit')); ?></a></td>
