2c4961f39b26b18785b95b4ef209b15e6f70e801
SignpostMarv <github@signpostmarv.name>
2012-05-01 01:13:46 +0100

    making abuse report review functional

--- templates/default/admin_abusereport_review.php
+++ templates/default/admin_abusereport_review.php
@@ -18,6 +18,8 @@ if(ctype_digit($id) === false){
 }else{
 	$id = (integer)$id;
 
+	$dirty = false;
+
 	try{
 		$AbuseReport = Globals::i()->WebUI->GetAbuseReport($id);
 	}catch(\Aurora\Addon\Exception $e){
@@ -30,7 +32,37 @@ if(ctype_digit($id) === false){
 		return;
 	}
 
+	if($_SERVER['REQUEST_METHOD'] === 'POST'){
+		if(isset($_POST['mark-complete']) === true){
+			Globals::i()->WebUI->AbuseReportMarkComplete($AbuseReport->Number());
+			$dirty = true;
+		}else if(isset($_POST['notes']) === true){
+			Globals::i()->WebUI->AbuseReportSaveNotes($AbuseReport->Number(), $_POST['notes']);
+			$dirty = true;
+		}
+	}
+
+	if($dirty === true){
+		try{
+			$AbuseReport = Globals::i()->WebUI->GetAbuseReport($AbuseReport->Number());
+		}catch(\Aurora\Addon\Exception $e){
+			header('HTTP/1.0 500 Internal Server Error');
+?>
+		<p class=problem><?php echo esc_html(__('Failed to get abuse report.')); ?><br><?php echo wp_kses(sprintf(__('An <em>%s</em> exception was thrown'), get_class($e)), array('em'=>array()))?>: <q><?php echo esc_html($e->getMessage()); ?></q></p>
+	</section>
+<?php
+			require_once('_footer.php');
+			return;
+		}
+	}
+
 	$pos = new Vector3($AbuseReport->ObjectPosition());
+
+	if($AbuseReport->Active() === false){
+?>
+		<p><?php echo esc_html(__('This abuse report has been closed.')); ?></p>
+<?php
+	}
 ?>
 		<details>
 			<summary><?php echo wp_kses(sprintf('%s: %s said <q>%s</q> near %s',
@@ -74,13 +106,11 @@ if(ctype_digit($id) === false){
 						<li><?php if(isset($FormProblems['notes']) === true){ ?><p class=problem><?php echo esc_html($FormProblems['notes']); ?></p><?php } ?><label for="abusereport_notes_<?php echo esc_attr($AbuseReport->Number()); ?>"><?php echo esc_html(__('Notes')); ?></legend><textarea id="abusereport_notes_<?php echo esc_attr($AbuseReport->Number()); ?>" name=notes><?php echo esc_html($AbuseReport->Notes()); ?></textarea></li>
 					</ol>
 				</fieldset>
-				<button type=submit><?php echo esc_html(__('Save Notes')); ?></button>
-			</form>
-			<form method=post>
-				<?php if(isset($FormProblems['nonce']) === true){ ?><p class=problem><?php echo esc_html($FormProblems['nonce']); ?></p><?php } ?><input type=hidden name=nonce value="<?php echo esc_attr(Globals::i()->Nonces->get(300)); ?>">
 				<ul>
-					<li><button type=submit name=delete><?php echo esc_html(__('Delete Report')); ?></button></li>
-					<li><button type=submit name=<?php echo esc_attr($AbuseReport->Active() ? 'deactivate' : 'activate'); ?>><?php echo esc_html($AbuseReport->Active() ? 'Deactive Abuse Report' : 'Activate Abuse Report'); ?></button></li>
+					<li><button type=submit><?php echo esc_html(__('Save Notes')); ?></button></li>
+<?php	if($AbuseReport->Active()){ ?>
+					<li><button type=submit name=mark-complete><?php echo esc_html(__('Mark abuse report as closed')); ?></button></li>
+<?php	} ?>
 				</ul>
 			</form>
 		</details>
