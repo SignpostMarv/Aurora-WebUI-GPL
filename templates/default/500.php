6252b231ec92339e008a77b0b1e9dd43055ae02b
SignpostMarv <github@signpostmarv.name>
2012-05-13 14:28:02 +0100

    Changing the format of API failure messages

--- templates/default/500.php
+++ templates/default/500.php
@@ -1,20 +1,56 @@
 <?php
-	header('HTTP/1.1 500 Internal Server Error');
-	require('_header-exception.php');
+header('HTTP/1.1 500 Internal Server Error');
+require('_header-exception.php');
 ?>
 	<section>
-		<h1><?php echo esc_html(__('Internal Server Error')); ?></h1>
-<?php if(isset($e)){
-		if($e instanceof Exception){ ?>
+<?php
+if(isset($e) && $e instanceof \Aurora\Addon\APIMethodException){
+?>
+		<hgroup>
+			<h1><?php echo esc_html(__('API Method Exception')); ?></h1>
+<?php
+	if($e instanceof \Aurora\Addon\APIAccessForbiddenException){
+?>
+			<h2><?php echo esc_html(__('API Access Forbidden')); ?></h2>
+		</hgroup>
+		<p class=problem><?php echo esc_html(sprintf(__('Access to API method %s was forbidden'), $e->GetAPIMethod())); ?>.</p>
+<?php
+	}else if($e instanceof \Aurora\Addon\APIAccessRateLimitException){
+?>
+			<h2><?php echo esc_html(__('API Access Forbidden')); ?></h2>
+		</hgroup>
+		<p class=problem><?php echo esc_html(sprintf(__('Access to API method %s temporarily suspended due to reaching the hourly limit.'), $e->GetAPIMethod())); ?>.</p>
+<?php
+	}else{
+?>
+
+<?php
+	}
+}else{
+?>
+		<hgroup>
+			<h1><?php echo esc_html(__('Internal Server Error')); ?></h1>
+<?php
+	if(isset($e)){
+		if($e instanceof \Aurora\Addon\WebUI\Exception || $e instanceof \libAurora\Exception || $e instanceof \Aurora\Addon\APIAccessFailedException){
+?>
+			<h2><?php echo esc_html(__('Exception message')); ?></h2>
+		</hgroup>
+		<p class=problem><?php echo esc_html($e->getMessage()); ?></p>
+<?php
+		}else{
+?>
+		</hgroup>
 		<p><?php echo esc_html(sprintf(__('There was an exception of type %1$s, code %2$u.'), get_class($e), $e->getCode())); ?></p>
-<?php	}
-		if($e instanceof \Aurora\Addon\WebUI\Exception || $e instanceof \libAurora\Exception || $e instanceof \Aurora\Addon\APIAccessFailedException){ ?>
-		<h2><?php echo esc_html(__('Exception message')); ?></h2>
-		<p><?php echo esc_html($e->getMessage()); ?></p>
-<?php	}
-	}else{ ?>
-		<p><?php echo esc_html(__('We apologise for the inconvinience, it appears there was an unknown error. Please try again later.')); ?></p>
-<?php } ?>
+<?php
+		}
+	}else{
+?>
+		<p class=problem><?php echo esc_html(__('We apologise for the inconvinience, it appears there was an unknown error. Please try again later.')); ?></p>
+<?php
+	}
+}
+?>
 	</section>
 <?php
 	require('_footer.php');
